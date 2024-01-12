ffab87e7a00d66b0aaf437a0c977f0ada071bb74
HeartBox12 <maximosity1@gmail.com>
2024-01-12 10:38:59 -0800

    Missed something
    Forgot to save something in previous commit. Amending.

--- baseState.gd
+++ baseState.gd
@@ -1,13 +1,13 @@
 extends State
 
-func enter():
+func enter(): #When this state is entered
 	pass
 	
-func exit():
+func exit(): #Just before this state is exited
 	pass
 
-func update(delta):
+func update(delta): #Equivalent to func process(delta) in the host. Only use process() to call this
 	pass
 
-func physics_update(delta):
+func physics_update(delta): #Equivalent to func physics_process() in the host.
 	pass
