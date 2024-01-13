85c4e24d5369c21b4313007a8eedc932471154ad
HeartBox12 <maximosity1@gmail.com>
2024-01-12 21:47:18 -0800

    Amended Sprite Selection

--- Player/State Scripts/Walk.gd
+++ Player/State Scripts/Walk.gd
@@ -1,12 +1,25 @@
-extends Node
+extends State
 @export var host:Node
+@export var sprite:Node
 
+func enter(): #When this state is entered
+	pass
+	
+func exit(): #Just before this state is exited
+	pass
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func update(delta): #Equivalent to func process(delta) in the host. Only use process() to call this
+	match host.facing: #Sprite Assign
+		0:
+			sprite.animation = "walk_right"
+		1:
+			sprite.animation = "walk_down"
+		2:
+			sprite.animation = "walk_left"
+		3:
+			sprite.animation = "walk_up"
+	#Change sprite to idle
+	#Start playing if needed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func physics_update(delta): #Equivalent to func physics_process() in the host.
 	pass
