0c96a7503a1e5ea1afba52400bd24b2c13c77cd7
HeartBox12 <maximosity1@gmail.com>
2024-01-12 23:00:08 -0800

    AnimationPlayer node added

---
+++ Map/main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+var day = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("startOfDay")
+
+func adjust_day():
+	$DayCount.text = "[center]The stars will alime in [color=#00FF00]%s days[/color][/center]" %[day]
+	$DayCount/TextShadow.text = "[center][color=#000000]The stars will alime in[/color][color=#003300] %s days[/color][/center]"%[day]
