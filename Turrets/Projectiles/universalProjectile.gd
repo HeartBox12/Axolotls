f947fa4aa69c5adabdeb37167642871879d9786b
TheMagmaPsychic <156372829+TheMagmaPsychic@users.noreply.github.com>
2024-01-13 01:31:30 -0700

    Added in projectiles

---
+++ Turrets/Projectiles/universalProjectile.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+var direction
+var speed:int = 1000 #how fast does the projectile go? in pixels/second
+var lifetime:float = 0.2 #how many seconds does the projectile say alive for
+var currentLife:float = 0.0 #current time it's been alive for
+signal bulletDestroyed() #unsure if this is needed, it would be to purge the variable from the array in the main code
+
+func _physics_process(delta):
+	currentLife += delta #makes sure that the projectile dies when it's lifetime is over
+	if currentLife >= lifetime:
+		queue_free() #destroy this node after this frame
+		bulletDestroyed.emit()
+	direction = global_transform.basis_xform(Vector2.DOWN) #gets a vector direction based on the rotation
+	position += direction * delta * speed #what it says on the tin; makes it move in the proper direction according to delta and speed
--- Turrets/universalTurret.gd
+++ Turrets/universalTurret.gd
@@ -1,8 +1,10 @@
 extends AnimatedSprite2D
 var fireCooldown:int = 1 #Easy access to change the cooldown period between shots, in seconds
 var fireDelay:float = 0.0 #Fire delay variable, current time between shots; this is the variable that is altered
-var target:Vector2 #Declares the target variable and restricts it to the Vector2 data type
-
+var target:Vector2 = Vector2(0, 0) #Declares the target variable and restricts it to the Vector2 data type
+var range:int = 100 #this is the range of this turret, will only fire on targets within this range
+var index:int
+signal fire(pos, rot, me)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,13 +12,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	target = get_global_mouse_position() #this is gonna be where the logic to find the nearest/first enemy in range goes, using mouse position for now
+	target = get_global_mouse_position() / $"..".scale.x #this is gonna be where the logic to find the nearest/first enemy in range goes, using mouse position for now
 	if fireDelay >= 0.05 * fireCooldown and fireDelay < 0.5 * fireCooldown: #Gives leeway between firing sprite and empty sprite
 		frame = 1 #Change to unloaded sprite
 	elif fireDelay >= 0.5 * fireCooldown: #If halfway through the cooldown show turret as loaded
 		frame = 0 #Change to loaded sprite
 	fireDelay += delta #Makes fireCooldown work regardless of computer speed
-	if fireDelay >= fireCooldown and position.distance_to(target) <= 100: #Can this turret fire? AKA is the fire cooldown done and is there a target in range?
+	if fireDelay >= fireCooldown and position.distance_to(target) <= range: #Can this turret fire? AKA is the fire cooldown done and is there a target in range?
 		fireBullet() #make turrret go pew
 
 func _physics_process(delta):
@@ -25,5 +27,5 @@ func _physics_process(delta):
 
 func fireBullet():
 	frame = 2 #Change to firing sprite
-	print("pew pew pew") #Projectile firing goes here
 	fireDelay = 0 #Resets the delay between shots
+	fire.emit(position, rotation, index)
