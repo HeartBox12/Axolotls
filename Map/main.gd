bc8f078dfe21b5adb776e344908abda6af3e0fce
HeartBox12 <maximosity1@gmail.com>
2024-01-13 14:22:01 -0800

    Tile indicator added
    This will help with future systems :)

--- Map/main.gd
+++ Map/main.gd
@@ -1,10 +1,16 @@
 extends Node2D
 var day = 2
+var playerPos #Player position in tilemap coords.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	
+
+func _process(delta):
+	#Get tilemap coords of tile the player is standing on
+	playerPos = $Tiles.local_to_map($Player.position)
+	$indicator.position = $Tiles.map_to_local(playerPos)
+
 func adjust_day():
 	$DayCount.text = "[center]The stars will alime in [color=#00FF00]%s days[/color][/center]" %[day]
 	$DayCount/TextShadow.text = "[center][color=#000000]The stars will alime in[/color][color=#003300] %s days[/color][/center]"%[day]
--- Map/tiles.gd
+++ Map/tiles.gd
@@ -1,8 +1,5 @@
 extends TileMap
 
-var playerScene = load("res://Player/player.tscn") #Identifies the player scene, loads it
-var player = playerScene.instantiate() #Identifies the player node
-
 var turretScene = load("res://Turrets/universalTurret.tscn") #Loads up the turret template
 var turrets:Array = [] #this is an array of all the turrets currently deployed, contains the direct node (use like "turrets[x].position = 64")
 
@@ -14,13 +11,16 @@ var turretPositions = 48 #temporary variable while I make sure turrets work prop
 func _ready():
 	game_start() #redirects to game_start() for when you lose and have to restart the game, won't have to reload the scene
 
-func game_start(): #When the game starts, add in the player and setup the UI
-	#add_child(player) #The player scene is loaded, this makes it a direct child of the tilemap
-	#player.position = Vector2(48, 48) #TEMPORARY Push the player into the field
+func game_start():
+	#NOTE: I want to use the animationPlayer and a script in Main for this
 	pass
 
 func _process(_delta):
-	if Input.is_action_just_pressed("test_button_to_make_something_happen"): #we'll get rid of this control in the final version, useful for testing
+	
+	
+	
+	#DEBUG: we'll get rid of this control in the final version, useful for testing
+	if Input.is_action_just_pressed("test_button_to_make_something_happen"): 
 		place_turret()
 
 func place_turret():
@@ -43,3 +43,4 @@ func fire(pos, rot, index):
 
 func bulletDestroyed():
 	pass
+
