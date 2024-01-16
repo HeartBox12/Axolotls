1120f1f3f5d28c9d0f9c95a3b57aac207e5e8be9
HeartBox12 <maximosity1@gmail.com>
2024-01-15 20:20:37 -0800

    Audio Added

--- Player/State Scripts/turret.gd
+++ Player/State Scripts/turret.gd
@@ -2,15 +2,19 @@ extends State
 @export var host:Node
 @export var machine:Node
 @export var clock:Node
+@export var riser:Node
+
 @export var wait_time:int
 
 func enter(): #When this state is entered
-	clock.max_value = wait_time #@export this, probs
+	clock.max_value = wait_time
 	clock.value = 0
 	clock.visible = true
+	riser.play()
 	
 func exit(): #Just before this state is exited
 	clock.visible = false
+	riser.stop()
 
 func update(delta): #Equivalent to func process(delta) in the host. Only use process() to call this
 	if host.input != Vector2(0, 0):
