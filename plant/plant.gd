53b640a9f02d10ca1f693bc902e9deeeb99583db
HeartBox12 <maximosity1@gmail.com>
2024-01-19 09:35:26 -0800

    Plant Sprites

--- plant/plant.gd
+++ plant/plant.gd
@@ -14,7 +14,7 @@ func ripen():
 		$sprite.play("ripen")
 		profit = 1
 	elif profit == 1: #The plant is ripe, and will mature
-		$sprite.play("mature")
+		$sprite.play("maturing")
 		profit = 3
 	elif profit == 3: #The plant is mature, and will die
 		health = 0
@@ -28,3 +28,5 @@ func _process(_delta):
 func _on_sprite_animation_finished():
 	if $sprite.animation == "ripen":
 		$sprite.play("ripe")
+	elif $sprite.animation == "maturing":
+		$sprite.play("mature")
