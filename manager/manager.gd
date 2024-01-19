eedba5e4984ad46724ea737f5e64a8c0d6b43681
HeartBox12 <maximosity1@gmail.com>
2024-01-19 13:37:34 -0800

    Export Added for testing purposes

--- manager/manager.gd
+++ manager/manager.gd
@@ -1,14 +1,18 @@
 extends Node2D
-
+var mainScene = preload("res://Map/maps.tscn")
+var instance:Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().paused = true
 
 func _on_start_pressed():
+	instance = mainScene.instantiate()
+	instance.position = Vector2(0, 0)
+	add_child(instance)
 	$AnimationPlayer.play("start")
+	get_tree().paused = false
 
 func _on_animation_finished(anim_name):
 	if anim_name == "start":
-		get_tree().paused = false
-		$Main._load()
+		instance._load()
